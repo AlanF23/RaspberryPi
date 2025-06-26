db292c4c59ba2f608ff3a5436bec578105af29f0
Alan Fucks <alanf200023unam@gmail.com>
2025-06-26 16:21:37 -0300

    cambios para que funque (mqtts sin tls)

--- clienteMqtt.py
+++ clienteMqtt.py
@@ -6,16 +6,16 @@ env = Env()
 env.read_env() #lee el archivo con las variables. por defecto .env
 
 async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    tls_context.verify_mode = ssl.CERT_REQUIRED
-    tls_context.check_hostname = True
-    tls_context.load_default_certs()
+    #tls_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    #tls_context.verify_mode = ssl.CERT_REQUIRED
+    #tls_context.check_hostname = True
+    #tls_context.load_default_certs()
 
     async with Client(
         env("SERVIDOR"),
-        protocol=ProtocolVersion.V31,
-        port=8883,
-        tls_context=tls_context,
+        #protocol=ProtocolVersion.V31,
+        #port=8883,
+        #tls_context=tls_context,
     ) as client:
         async with client.messages() as messages:
             await client.subscribe("#")
